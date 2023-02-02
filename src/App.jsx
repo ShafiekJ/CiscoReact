878e8c6dd08139135fd3250582c716835f50c91f
ShafiekJ <shafeeqjabaar@gmail.com>
2023-02-02 15:09:06 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Textbox from './components/Textbox'
+import Text from './components/Text'
+import './App.css'
+
+function App() {
+ 
+ 
+  return (
+    <div className="App">
+        <Textbox/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Mira.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import mira from '../assets/Mira_Smile.png'
+export default function Mira() {
+  return (
+    <div><img src = {mira}/></div>
+  )
+}
